ae242c09e4924dc1b434bffa3b5eeeaa2d45b62e
Gerald <mayhew2006@gmail.com>
2026-05-13 21:52:31 +0000

    add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,36 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Grace",
-  description: "Someone who listens when no one else is there.",
-  manifest: "/manifest.json",
+  description: "Technology that feels more human.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q4J04MDK1F"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Q4J04MDK1F');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
