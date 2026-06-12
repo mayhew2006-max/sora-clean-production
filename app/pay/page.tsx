87a250400b92d60b916f020c8dd17816a4996203
Gerald <mayhew2006@gmail.com>
2026-06-12 23:51:40 +0000

    Fix active Grace Stripe checkout link

--- app/pay/page.tsx
+++ app/pay/page.tsx
@@ -1,4 +1,4 @@
-const STRIPE_LINK = "https://buy.stripe.com/3cI4gAcfD3sVaIiflI1gs02;
+const STRIPE_LINK = "https://buy.stripe.com/3cI4gAcfD3sVaIiflI1gs02";
 
 export default function Pay() {
   return (
